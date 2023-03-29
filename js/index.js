3060d9b0012ca097ec09176febd0d3201f890a47
bigman <terrence.chebii@student.moringaschool.com>
2023-03-29 22:04:24 +0300

    partially done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,50 @@
-document.addEventListener("DOMContentLoaded", function() {});
+
+function showNames(names){
+    names.forEach(books => {
+        let list = document.querySelector('#list')
+        let title = document.createElement('li')
+        title.innerHTML = `
+        <h2>${books.title}</h2>
+        `
+        list.appendChild(title)
+        
+        title.addEventListener('click' , () => {
+            let details = document.querySelector('#show-panel')
+            let li = document.createElement('li')
+            li.innerHTML = `
+            <img src='${books.img_url}'><br>
+            <h1>${books.title}</h1><br>
+            <h3>${books.subtitle}</h3>
+            <h3>${books.description}</h3>
+            <h6>${books.author}</h6>
+            <button ${books.id}>like</button>
+
+            <h2 id='disp'><h2>
+            `
+            details.appendChild(li)
+
+            let btn = li.querySelector('button')
+            btn.addEventListener('click' , vote)
+
+            function vote(data){
+                fetch('http://localhost:3001/users')
+                .then(res => res.json())
+                .then(data => vote(data))
+
+                data.forEach(users => {
+                let disp = li.querySelector('h2')
+                disp.innerHTML = `
+                ${users.username}
+                `
+            })
+            }
+        })
+    });
+}
+
+fetch('http://localhost:3001/books')
+.then(res => res.json())
+.then(data => showNames(data))
+
+
+
